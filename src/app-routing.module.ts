98b483bdc0864e898decd9a4abff1ae3ccb62fe6
amritofficial <itsamrit1996@gmail.com>
2018-02-21 18:44:32 -0500

    Wallpapers changed

--- src/app-routing.module.ts
+++ src/app-routing.module.ts
@@ -2,6 +2,7 @@ import { Routes } from "@angular/router";
 import { ProfileComponent } from "./app/profile/profile.component";
 import { NavTimelineComponent } from "./app/nav-timeline/nav-timeline.component";
 import { NavWorkComponent } from "./app/nav-work/nav-work.component";
+import { NavEducationComponent } from "./app/nav-education/nav-education.component";
 
 // export const appRoutes: Routes = [
 //     {path: '', redirectTo: '/timeline', pathMatch: 'full'},
@@ -14,7 +15,8 @@ export const appRoutes: Routes = [
     {path: 'profile', component:ProfileComponent,
         children: [
             {path: 'timeline', component: NavTimelineComponent, outlet: 'auxoutlet'},
-            {path: 'work', component: NavWorkComponent, outlet: 'auxoutlet'}
+            {path: 'work', component: NavWorkComponent, outlet: 'auxoutlet'},
+            {path: 'education', component: NavEducationComponent, outlet: 'auxoutlet'}
         ]
     }
 ];
